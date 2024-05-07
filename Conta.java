84a08ef72b6f40795f7db31b0215d749f5a3411d
Th4yss4 <thayssananda@ufpi.edu.br>
2024-05-06 23:52:39 -0300

    atualizar classe conta add vinculo com cliente #2

--- Conta.java
+++ Conta.java
@@ -1,7 +1,34 @@
-class Conta {
-    private double saldo;
+import java.util.ArrayList;
 
-    public Conta() {
+class Conta {
+    private Cliente cliente;
+    private double saldo;
+    private ArrayList<Transacao> transacoes;
+
+    public Conta(Cliente cliente) {
+        this.cliente = cliente;
         this.saldo = 0.0;
+        this.transacoes = new ArrayList<>();
     }
+
+    public void realizarSaque(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            transacoes.add(new Transacao("Saque", -valor));
+            System.out.println("Saque de " + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+
+    public void cadastrarCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void abrirConta() {
+        System.out.println("Conta aberta para o cliente " + cliente.getNome() + ".");
+    }
+
+    //Add transações
+
 }

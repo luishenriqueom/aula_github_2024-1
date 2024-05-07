5e3db980afc4b2c353e6c80aad0960a93955790c
LucasjsSilva <luquinhajssilva@ufpi.edu.br>
2024-05-07 08:14:14 -0300

    main atualizada #8

--- Main.java
+++ Main.java
@@ -1,39 +1,76 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Menu mainMenu = new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-        int opcao = mainMenu.getSelection();
+	public static void main(String[] args) {
+		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Cadastrar Cliente", "Abrir Conta", "Realizar Saque", "Relatório de Transações"));
+		int opcao = mainMenu.getSelection();
         
         switch (opcao) {
-            case 1:
-                // Lógica para cadastro de conta
+            case 1:                
+			 	cadastrarCliente();
                 break;
             case 2:
-                cadastrarCliente();
+				abrirConta();
                 break;
             case 3:
-                // Lógica para operações
+                realizarSaque();
+                break;
+			case 4:
+                relatorioTransacoes();
                 break;
             default:
                 System.out.println("Opção inválida!");
         }
         
-        System.out.println("Fim");
-    }
+    System.out.println("Fim");
+}
 
-    private static void cadastrarCliente() {
-        System.out.println("Cadastro de Cliente:");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o nome do cliente:");
-        String nome = scanner.nextLine();
-        System.out.println("Digite o CPF do cliente:");
-        String cpf = scanner.nextLine();
-        Cliente cliente = new Cliente(nome, cpf);
-        System.out.println("Cliente cadastrado com sucesso:");
-        System.out.println("Nome: " + cliente.getNome());
-        System.out.println("CPF: " + cliente.getCpf());
-    }
+
+private static void cadastrarCliente() {
+	System.out.println("Cadastro de Cliente:");
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Digite o nome do cliente:");
+	String nome = scanner.nextLine();
+	System.out.println("Digite o CPF do cliente:");
+	String cpf = scanner.nextLine();
+	Cliente cliente = new Cliente(nome, cpf);
+	System.out.println("Cliente cadastrado com sucesso:");
+	System.out.println("Nome: " + cliente.getNome());
+	System.out.println("CPF: " + cliente.getCpf());
+}
+private static void abrirConta() {
+	System.out.println("Abrir Conta:");
+	String nome = "João";
+	String cpf = "123";
+
+	Cliente cliente = new Cliente(nome, cpf);
+
+	Conta conta = new Conta(cliente);
+
+	conta.abrirConta();
+}
+private static void realizarSaque(){
+	System.out.println("Saque:");
+	String nome = "João";
+	String cpf = "123";
+
+	Cliente cliente = new Cliente(nome, cpf);
+
+	Conta conta = new Conta(cliente);
+
+	conta.realizarSaque(100.0);
+}
+private static void relatorioTransacoes(){
+	System.out.println("Transações:");
+	String nome = "João";
+	String cpf = "123";
+
+	Cliente cliente = new Cliente(nome, cpf);
+
+	Conta conta = new Conta(cliente);
+
+	conta.gerarRelatorioTransacoes();
+}
 }

d6b292e5ed3d8da6f537ee5bb844a778fc6ddf9c
Th4yss4 <thayssananda@ufpi.edu.br>
2024-05-06 23:39:24 -0300

    add transacao #7

---
+++ Transacao.java
@@ -0,0 +1,10 @@
+class Transacao {
+    private String descricao;
+    private double valor;
+
+    public Transacao(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+    
+}

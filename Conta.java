875028e09f8eecabb29a8a5056a2d721893c2c87
Luis Henrique <luishenriqueom@ufpi.edu.br>
2024-05-07 00:04:20 -0300

    adicionado funcao de gerar relatorio de transacoes#4

--- Conta.java
+++ Conta.java
@@ -1,7 +1,17 @@
 class Conta {
     private double saldo;
+    private ArrayList<Transacao> transacoes;
 
     public Conta() {
         this.saldo = 0.0;
+        this.transacoes = new ArrayList<>();
+    }
+
+    public void gerarRelatorioTransacoes() {
+        System.out.println("Relatório de transações para o cliente " + cliente.getNome() + ":");
+        for (Transacao transacao : transacoes) {
+            System.out.println(transacao.getDescricao() + ": " + transacao.getValor());
+        }
+        System.out.println("Saldo atual: " + saldo);
     }
 }
